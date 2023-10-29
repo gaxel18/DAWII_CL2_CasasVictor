f80c02eac1839b585bfc6b7e91ad6edde5adac7a
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-10-28 19:19:37 -0500

    Initial commit

---
+++ src/main/java/edu/cibertec/edu/pe/DAWII_CL2_CasasVictor/DawiiCl2CasasVictorApplication.java
@@ -0,0 +1,13 @@
+package edu.cibertec.edu.pe.DAWII_CL2_CasasVictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2CasasVictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2CasasVictorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cibertec/edu/pe/DAWII_CL2_CasasVictor/DawiiCl2CasasVictorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cibertec.edu.pe.DAWII_CL2_CasasVictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2CasasVictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
