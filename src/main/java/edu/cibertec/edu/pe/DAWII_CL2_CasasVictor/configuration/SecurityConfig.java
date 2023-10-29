c858c07e255d72165dfeff875b4ed654b99ebe4f
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-10-28 19:46:49 -0500

    application.properties , Service , Controller y SecurityConfig

---
+++ src/main/java/edu/cibertec/edu/pe/DAWII_CL2_CasasVictor/configuration/SecurityConfig.java
@@ -0,0 +1,55 @@
+package edu.cibertec.edu.pe.DAWII_CL2_CasasVictor.configuration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import edu.cibertec.edu.pe.DAWII_CL2_CasasVictor.service.DetalleUsuarioService;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final DetalleUsuarioService detalleUsuarioService;
+
+    @Bean
+    public SecurityFilterChain config(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        auth ->
+                                auth.requestMatchers("/auth/login",
+                                                "/auth/registrar",
+                                                "/auth/guardarUsuario",
+                                                "/resources/**",
+                                                "/static/**",
+                                                "/styles/**",
+                                                "/scripts/**")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated()
+                ).formLogin(
+                        login ->
+                                login.loginPage("/auth/login")
+                                        .defaultSuccessUrl("/auth/login-success")
+                                        .usernameParameter("nomusuario")
+                                        .passwordParameter("password")
+                ).authenticationProvider(authenticationProvider());
+        return http.build();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(detalleUsuarioService);
+        authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return authenticationProvider;
+    }
+}
+
